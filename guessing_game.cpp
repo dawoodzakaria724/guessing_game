592f39447a2af84d5a8348d48778269cb0431c70
Dawood <dawoodzakaria72@gmail.com>
2020-05-08 16:13:04 -0700

    refactoring

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -6,6 +6,37 @@
 #include <array>
 #include <fstream>
 
+void save_score(int count)
+{
+
+	std::ifstream input("best_score.txt");
+	if (!input.is_open())
+	{
+		std::cout << "Unable to read file\n";
+		return;
+	}
+
+	int best_score;
+	input >> best_score;
+
+	std::ofstream output("best_score.txt");
+
+	if (!output.is_open())
+	{
+		std::cout << "Unable to read file\n";
+		return;
+	}
+
+	if (count < best_score)
+	{
+		output << count;
+	}
+	else
+	{
+		output << best_score;
+	}
+
+}
 
 void print_vector(std::vector<int> vector)
 {
@@ -47,32 +78,8 @@ void play_game()
 		}
 	}
 
-	std::ifstream input("best_score.txt");
-	if(!input.is_open())
-	{ 
-		std::cout << "Unable to read file\n";
-		return;
-	}
+	save_score(count);
 
-	int best_score;
-	input >> best_score;
-
-	std::ofstream output("best_score.txt");
-
-	if (!output.is_open())
-	{
-		std::cout << "Unable to read file\n";
-		return;
-	}
-
-	if (count < best_score)
-	{
-		output << count;
-	}
-	else
-	{
-		output << best_score;
-	}
 
 	print_vector(guesses);
 }
